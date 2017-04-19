f29c7bff724d035d6f2e36c83e37d80c5d0bc8c4
LeviLee <1219571443@qq.com>
2017-04-19 16:21:12 +0800

    copy uitl function from vue
    copy uitl function from vue

--- src/util/index.js
+++ src/util/index.js
@@ -18,10 +18,77 @@ export function initCtx(van) {
 }
 
 // merge two object
-export function mergeTo(obj1, obj2) {
-  for (var key in obj1) {
-    var vo = obj1[key];
-    obj2[key] = vo;
+export function mergeTo(from, to) {
+  for (var key in from) {
+    var vo = from[key];
+    to[key] = vo;
   }
-  return obj2;
+  return to;
+}
+
+export function isUndef(v) {
+  return v === undefined || v === null;
+}
+
+export function isDef(v) {
+  return v !== undefined && v !== null;
+}
+
+export function isTrue(v) {
+  return v === true;
+}
+
+/**
+ * Check if value is primitive
+ */
+export function isPrimitive(value) {
+  return typeof value === 'string' || typeof value === 'number';
+}
+
+/**
+ * Strict object type check. Only returns true
+ * for plain JavaScript objects.
+ */
+export function isPlainObject(obj) {
+  return Object.prototype.toString.call(obj) === '[object Object]';
+}
+
+/**
+ * Simple bind, faster than native
+ */
+export function bind(fn, ctx) {
+  function boundFn(a) {
+    const l = arguments.length;
+    return l
+      ? l > 1
+        ? fn.apply(ctx, arguments)
+        : fn.call(ctx, a)
+      : fn.call(ctx);
+  }
+  // record original fn length
+  boundFn._length = fn.length;
+  return boundFn;
+}
+
+/**
+ * Mix properties into target object.
+ */
+export function extend(to, _from) {
+  for (const key in _from) {
+    to[key] = _from[key];
+  }
+  return to;
+}
+
+/**
+ * Ensure a function is called only once.
+ */
+export function once(fn) {
+  let called = false;
+  return function() {
+    if (!called) {
+      called = true;
+      fn.apply(this, arguments);
+    }
+  };
 }
